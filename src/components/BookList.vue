53b5202c6e25c7cd4e437577f1398d62f6e54ad0
AreejAlmalki <AreejAlmalki1@hotmail.com>
2021-01-08 09:27:18 +0300

    second

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -31,7 +31,7 @@ export default {
     BookForm
   },
   methods: {
-    appendBook(bookTitle, bookAuthor) {
+    appendBook(BoookData) {
       this.books.push({ title: bookData.bookTitle,
         author: bookData.bookAuthor,
         finishedReading: bookData.finishedReading });
